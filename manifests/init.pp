3e68b7de38368c8f39b6ae163460e29b02573b1c
Jordi Prats <jprats@systemadmin.es>
2016-11-25 11:58:28 +0100

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 class cups(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits cups::params{
+            $manage_package        = true,
+            $package_ensure        = 'installed',
+            $manage_service        = true,
+            $manage_docker_service = true,
+            $service_ensure        = 'running',
+            $service_enable        = true,
+          ) inherits cups::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
